31a01b1ba01d279ea159b32b8a5ac41be3bfdada
Gerardo Barragan <s_tu_ar98@hotmail.com>
2022-11-25 21:31:40 -0600

    creando servicio para buscar los Gifs

--- src/app/gifs/buscador/buscador.component.ts
+++ src/app/gifs/buscador/buscador.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-buscador',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   ]
 })
 export class BuscadorComponent {
+
+  constructor(private gifsService: GifsService) { } //inyección del servicio
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>; //referencia al input
+
+  buscar(){
+    const valor: string = this.txtBuscar.nativeElement.value;
+    this.txtBuscar.nativeElement.value = ''; //borrar el texto del input
+
+    if(valor.trim().length === 0) { //si el input esta vacío 
+      return;
+    }
+
+    this.gifsService.buscarGifs( valor ) //uso del servicio
+
+  }
 
 }
